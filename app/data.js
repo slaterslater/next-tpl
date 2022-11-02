9b7a088a205e7e9114955a1dc57b832c7380c579
slaterslater <slaterslater@gmail.com>
2022-11-02 14:35:45 -0400

    awaits json before return

--- app/data.js
+++ app/data.js
@@ -1,7 +1,10 @@
 // data.js
 
-export async function getData(endpoint, revalidate = 0) {
+// export async function getData(endpoint, revalidate = 0) {
+export async function getData(endpoint) {
   const url = `${process.env.API_BASE}/${endpoint}`
-  const data = await fetch(url, { next: { revalidate } });
-  return data.json()
+  // const data = await fetch(url, { next: { revalidate } });
+  const res = await fetch(url);
+  const data = await res.json();
+  return data
 }
