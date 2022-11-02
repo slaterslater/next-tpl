c0250e2620330d46f63644617edf86aabc347bba
slaterslater <slaterslater@gmail.com>
2022-11-02 14:50:13 -0400

    testing the endpoint

--- app/page.js
+++ app/page.js
@@ -14,6 +14,7 @@ const {API_BASE, LEAGUE_ID} = process.env
 
 export default async function Page() {
   // const leagueGames = await getLeagueGames()
-  const leagueGames = await getData(`games/${LEAGUE_ID}`)
+  // const leagueGames = await getData(`games/${LEAGUE_ID}`)
+  const leagueGames = await getData(`gameEvents/46953/9328`)
   return <Schedule season={leagueGames} />;
 }
