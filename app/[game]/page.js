a4c684a9918fa995add007e807a9357f23fa660b
slaterslater <slaterslater@gmail.com>
2022-11-02 14:18:02 -0400

    uses getData function on all pages

--- app/[game]/page.js
+++ app/[game]/page.js
@@ -1,25 +1,26 @@
-import { getData } from "../data";
+// import { getData } from "../data";
 import Stats from "./Stats";
 
 import styles from '../../styles/Game.module.css'
 import Link from "next/link";
+import { getData } from "../data";
 
-const {API_BASE} = process.env 
+// const {API_BASE} = process.env 
 
-async function getGameEvents(path) {
-  const url = `${API_BASE}/gameEvents/${path}`
-  const events = await fetch(url);
-  // const events = await getData(`gameEvents/${path}`)
-  // const score = events.reduce((goals, { eventType }) => {
-  //   if (eventType === "Goal") goals += 1
-  //   return goals
-  // }, 0);
-  // return {
-  //   score,
-  //   events
-  // }
-  return events.json()
-}
+// async function getGameEvents(path) {
+//   const url = `${API_BASE}/gameEvents/${path}`
+//   const events = await fetch(url);
+//   // const events = await getData(`gameEvents/${path}`)
+//   // const score = events.reduce((goals, { eventType }) => {
+//   //   if (eventType === "Goal") goals += 1
+//   //   return goals
+//   // }, 0);
+//   // return {
+//   //   score,
+//   //   events
+//   // }
+//   return events.json()
+// }
 
 // maybe req should return events.pop()
 // 2 state variables 
@@ -31,8 +32,10 @@ export default async function GamePage({ params, searchParams }) {
   const [gameId, awayId, homeId] = params.game.split("-");
   // const awayTeamData = await getGameEvents(`${gameId}/${awayId}`)
   // const homeTeamData = await getGameEvents(`${gameId}/${homeId}`)
-  const awayEvents = await getGameEvents(`${gameId}/${awayId}`)
-  const homeEvents = await getGameEvents(`${gameId}/${homeId}`)
+  // const awayEvents = await getGameEvents(`${gameId}/${awayId}`)
+  // const homeEvents = await getGameEvents(`${gameId}/${homeId}`)
+  const awayEvents = await getData(`gameEvents/${gameId}/${awayId}`)
+  const homeEvents = await getData(`gameEvents/${gameId}/${homeId}`)
   const eventData = {
     awayId,
     homeId,
--- app/page.js
+++ app/page.js
@@ -1,17 +1,19 @@
 // page.js
 // Homepage
 
+import { getData } from "./data";
 import Schedule from "./Schedule";
 
 const {API_BASE, LEAGUE_ID} = process.env
 
-async function getLeagueGames() {
-  const url = `${API_BASE}/games/${LEAGUE_ID}`
-  const leagueGames = await fetch(url);
-  return leagueGames.json();
-}
+// async function getLeagueGames() {
+//   const url = `${API_BASE}/games/${LEAGUE_ID}`
+//   const leagueGames = await fetch(url);
+//   return leagueGames.json();
+// }
 
 export default async function Page() {
-  const leagueGames = await getLeagueGames()
+  // const leagueGames = await getLeagueGames()
+  const leagueGames = await getData(`games/${LEAGUE_ID}`)
   return <Schedule season={leagueGames} />;
 }
