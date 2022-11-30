80fe20c6dacc47d5b76c746701eb8250cb6962ee
slaterslater <slaterslater@gmail.com>
2022-11-30 00:31:38 -0500

    adds description

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,7 @@ export default async function RootLayout({ children }) {
     <html lang="en">
       <head>
         <title>TPL scores</title>
+        <meta name="description" content="track live TPL scores"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </head>
       <body>
