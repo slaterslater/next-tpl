a5f8311347d9519f9e0a5b25129537a131727a33
slaterslater <slaterslater@gmail.com>
2024-10-11 15:26:32 -0400

    adds google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 // layout.js
 // Root layout
 
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { GameProvider } from "./context/gameContext";
 import './global.css';
 
@@ -12,11 +13,13 @@ export default async function RootLayout({ children }) {
         <meta name="description" content="track live TPL scores"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </head>
+      
       <body>
         <GameProvider>
           {children}
         </GameProvider>
       </body>
+      <GoogleAnalytics gaId={`${process.env.NEXT_PUBLIC_GAID}`} />
     </html>
   );
 }
