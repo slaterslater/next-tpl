74f7d0d64d630414d34a7d9aa5b1d2f74cdd8e0b
slaterslater <slaterslater@gmail.com>
2025-02-05 12:18:04 -0500

    removes prefetch for GameLink

--- app/components/GameLink.js
+++ app/components/GameLink.js
@@ -5,7 +5,7 @@ export default function GameLink({game, weekNum, gameNum}) {
     const [hh, mm] = game.time.split("-")[0].split(":");
     
     return (
-      <Link className="game" href={`/week/${weekNum}/game/${gameNum}`}>
+      <Link className="game" href={`/week/${weekNum}/game/${gameNum}`} prefetch={false}>
         <div className="field">
           <span>{`${hh % 12}:${mm}`}</span>
           <span>{game.location}</span>
