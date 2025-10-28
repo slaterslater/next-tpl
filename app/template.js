d7bd800dd910139c2546f28e47b0afa7b473a697
slaterslater <slaterslater@gmail.com>
2025-10-28 13:11:32 -0400

    removes force-static

--- app/template.js
+++ app/template.js
@@ -2,7 +2,7 @@ import { LeagueProvider } from "./context/leagueContext";
 import { getData } from './utils/lib';
 
 export const revalidate = 14400;
-export const dynamic = 'force-static';
+// export const dynamic = 'force-static';
 
 export default async function Template({ children }) {
   const ID = process.env.NEXT_PUBLIC_LEAGUE_ID
