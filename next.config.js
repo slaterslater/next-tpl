26933784923d7ee0147dc1a886e3bf4f0d740d60
slaterslater <slaterslater@gmail.com>
2025-11-02 21:31:30 -0500

    updates config and toml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 }
- 
+
 module.exports = nextConfig
